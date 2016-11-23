cd433363f9b51a050b61b7720a96861c7aeae9a4
jeremybeck <jeremy_beck@me.com>
2016-11-22 21:04:51 -0700

    Added first draft of unique property identification

--- capstone_project/scripts/00_blight_load_and_format.R
+++ capstone_project/scripts/00_blight_load_and_format.R
@@ -36,7 +36,7 @@ crime_dat <- read.csv('./data/detroit-crime.csv', header=T, na.strings='', strin
 call311_dat$geohash <- gh_encode(call311_dat$lat, call311_dat$lng, 10)
 call311_dat$unique_key <- paste0('call311_',seq(1,nrow(call311_dat)))
 
-
+message('Violation Address has 22k with incorrect lat/long.  maybe look into using MailingAddress')
 blight_viols <- blight_viols %>%
 	mutate(coord = str_extract_all(ViolationAddress, "\\([^()]+\\)")) %>%
 	transform(coord = gsub('[()]','', coord)) %>%
@@ -60,7 +60,26 @@ crime_dat$unique_key <- paste0('crime_',seq(1,nrow(crime_dat)))
 # Create a Comprehensive List of Unique Properties
 #--------------------------------------------------
 
+all_props <- call311_dat %>%
+	select(geohash, unique_key) %>% # 311 address is 'address'
+	rbind({ blight_viols %>% select(geohash, unique_key)}) %>% # ViolationAddress
+	rbind({ demo_permits %>% select(geohash, unique_key)}) %>% # site_location
+	rbind({ crime_dat %>% select(geohash, unique_key)}) # ADDRESS 
 
+length(unique(all_props$geohash))
 
+unique_props <- all_props %>% dplyr::select(geohash) %>% unique()
 
+# Check how frequently different properties appear
+all_props %>% 
+	group_by(geohash) %>%
+	summarize(COUNT = n()) %>%
+	arrange(desc(COUNT)) %>%
+	head(20)
+
+# Parcel File obtained from the URL:
+# https://data.detroitmi.gov/Property-Parcels/Parcel-Points-Ownership/eijm-6nr4/data
+# This was used to map latitude and longitude to parcel number for missing permit data
+# and also contains a wealth of information about sales prices, improvements, and cost
+# per sq. foot. 
 																		
