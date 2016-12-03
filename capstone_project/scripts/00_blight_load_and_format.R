00324c416f247b8524c286f806c283f915af1632
jeremybeck <jeremy_beck@me.com>
2016-12-02 21:48:48 -0700

    Downloaded new data files from Detroit Data portal and modified script
    Using parcel_data file as default list of properties with flag from
    demolition data

--- capstone_project/scripts/00_blight_load_and_format.R
+++ capstone_project/scripts/00_blight_load_and_format.R
@@ -32,17 +32,24 @@ retrieve_buildings <- function(lat, lng, dataset) {
 #-----------------------------
 
 # 311 Call Data
-call311_dat <- read.csv('./data/detroit-311.csv', header=T, na.strings='', stringsAsFactors=F)
+# Downloaded from https://data.detroitmi.gov/Government/Improve-Detroit-Submitted-Issues/fwz3-w3yn
+call311_dat <- read.csv('./data/Improve_Detroit__Submitted_Issues.csv', header=T, na.strings='', stringsAsFactors=F)
 
 # Blight Violations
-blight_viols <- read.csv('./data/detroit-blight-violations.csv', header=T, na.strings='', stringsAsFactors=F)
+# Downloaded from https://data.detroitmi.gov/Property-Parcels/Blight-Violations/teu6-anhh
+blight_viols <- read.csv('./data/Blight_Violations.csv', header=T, na.strings='', stringsAsFactors=F)
 
 # Demo Permits
-demo_permits <- read.csv('./data/detroit-demolition-permits.tsv', header=T, sep='\t', na.strings='', stringsAsFactors=F)
+# Downloaded from https://data.detroitmi.gov/Property-Parcels/Building-Permits/xw2a-a7tf
+demo_permits <- read.csv('./data/Building_Permits.csv', header=T, stringsAsFactors=F)
 
 # Crime Data
-crime_dat <- read.csv('./data/detroit-crime.csv', header=T, na.strings='', stringsAsFactors=F)
+# Downloaded from https://data.detroitmi.gov/Public-Safety/DPD-All-Crime-Incidents-2009-Present-Provisional-/b4hw-v6w2
+crime_dat <- read.csv('./data/DPD__All_Crime_Incidents__2009_-_Present__Provisional_.csv', header=T, na.strings='', stringsAsFactors=F)
 
+# Detroit Land Parcels
+# Downloaded from https://data.detroitmi.gov/Property-Parcels/Parcel-Points-Ownership/eijm-6nr4
+parcel_data <- read.csv('./data/Parcel_Points_Ownership.csv', header=T, sep=',', na.strings='', stringsAsFactors=F)
 
 #----------------------------
 # Geohash Lat/Long Pairs
@@ -57,15 +64,17 @@ call311_dat$unique_key <- paste0('call311_',seq(1,nrow(call311_dat)))
 
 message('Violation Address has 22k with incorrect lat/long.  maybe look into using MailingAddress')
 blight_viols <- blight_viols %>%
-	mutate(coord = str_extract_all(MailingAddress, "\\([^()]+\\)")) %>%
+	mutate(coord = str_extract_all(ViolationLocation, "\\([^()]+\\)")) %>%
 	transform(coord = gsub('[()]','', coord)) %>%
 	separate(coord, c('lat', 'lng'), ', ') %>%
 	mutate(geohash = gh_encode(as.numeric(lat), as.numeric(lng), 8),
 				 unique_key = paste0('blightviol_',row_number()))
 
 demo_permits <- demo_permits %>%
+	filter(BLD_PERMIT_TYPE == 'Dismantle') %>%
 	mutate(coord = str_extract_all(site_location, "\\([^()]+\\)")) %>%
-	transform(coord = gsub('[()]','', coord)) %>%
+	transform(coord = gsub('[()]','', coord),
+						SITE_ADDRESS = gsub("\\s+", " ", SITE_ADDRESS)) %>%
 	separate(coord, c('lat', 'lng'), ', ') %>%
 	mutate(geohash = gh_encode(as.numeric(lat), as.numeric(lng), 8),
 				 unique_key = paste0('demo_',row_number()))
@@ -82,43 +91,38 @@ all_props <- call311_dat %>%
 	select(geohash, unique_key) %>% mutate(source = 'call311') %>% # 311 address is 'address'
 	rbind({ blight_viols %>% select(geohash, unique_key) %>% mutate(source = 'viols')}) %>% # ViolationAddress
 	rbind({ demo_permits %>% select(geohash, unique_key) %>% mutate(source = 'demos')}) %>% # site_location
-	rbind({ crime_dat %>% select(geohash, unique_key) %>% mutate(source = 'crimes')}) # ADDRESS 
+	rbind({ crime_dat %>% select(geohash, unique_key) %>% mutate(source = 'crimes')}) # ADDRESS
 
 length(unique(all_props$geohash))
 
 unique_props <- all_props %>% dplyr::select(geohash) %>% unique()
 
 # Check how frequently different geohashes appear in data sets
-test <- all_props %>% 
+test <- all_props %>%
 	group_by(geohash) %>%
 	mutate(HASH_COUNT = n()) %>%
 	ungroup() %>%
 	group_by(geohash, source) %>%
 	summarize(HASH_COUNT = max(HASH_COUNT),
-						SOURCE_COUNT = n()) %>% 
+						SOURCE_COUNT = n()) %>%
 	arrange(desc(HASH_COUNT), geohash, desc(SOURCE_COUNT))
 
-# Parcel File obtained from the URL:
-# https://data.detroitmi.gov/Property-Parcels/Parcel-Points-Ownership/eijm-6nr4/data
-# This was used to map latitude and longitude to parcel number for missing permit data
-# and also contains a wealth of information about sales prices, improvements, and cost
-# per sq. foot. 
-
-detroit_data <- read.csv('./data/Parcel_Points_Ownership.csv', header=T, sep=',', na.strings='', stringsAsFactors=F)
-
-detroit_data <- detroit_data %>%
+# Parcel List with flags from Demo Data by ParcelNo
+parcel_data <- parcel_data %>%
+	filter(!is.na(ParcelNo)) %>%
 	transform(Latitude = as.numeric(Latitude),
 						Longitude = as.numeric(Longitude)) %>%
-	mutate(geohash = gh_encode(Latitude, Longitude, 8))
+	mutate(geohash = gh_encode(Latitude, Longitude, 8),
+				 demo_flag = ifelse(ParcelNo %in% demo_permits$PARCEL_NO, 1, 0))
 
-detroit_data %>% 
+junk <- parcel_data %>% 
 	group_by(geohash) %>%
 	dplyr::summarize(COUNT = n()) %>%
 	arrange(desc(COUNT)) %>%
 	head(10)
 
 
-	
-	
-	
+
+
+
 																		
